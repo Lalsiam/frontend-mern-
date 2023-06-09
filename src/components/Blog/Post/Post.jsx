f76bd23e82c647befc9b68d0b56e5101429926a2
Lalsiam <55026956+Lalsiam@users.noreply.github.com>
2023-06-09 14:01:16 +0530

    Update Post.jsx images

--- src/components/Blog/Post/Post.jsx
+++ src/components/Blog/Post/Post.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import "./post.css";
 
 export default function Post({ post }) {
-  const PF = "http://localhost:5000/images/";
+  const PF = "https://backend-pasn.onrender.com/images/";
   return (
     <>
       <Link to={`/post/${post._id}`} className="link">
