d385a467cc68b8bed06969a0e53a79f19340b939
Lalsiam <55026956+Lalsiam@users.noreply.github.com>
2023-06-09 12:40:41 +0530

    Update Upload.jsx api

--- src/components/Video/Upload.jsx
+++ src/components/Video/Upload.jsx
@@ -151,7 +151,7 @@ const Upload = ({ setOpen }) => {
 
   const handleUpload = async (e) => {
     e.preventDefault();
-    const res = await axios.post("/api/videos", { ...inputs, tags });
+    const res = await axios.post("https://backend-pasn.onrender.com/api/videos", { ...inputs, tags });
     setOpen(false);
     res.status === 200 && navigate(`/video/${res.data._id}`);
   };
